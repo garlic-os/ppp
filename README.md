36828ed13085c995b18998093454daf63de2b519
Nate <14845347+garlic-os@users.noreply.github.com>
2023-10-06 18:23:57 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# PPP - Pesterchum Pidgin Plugin
+Adapts Pesterchum markup to Pidgin markup
+
+
+
+---
+this is all [virtueisdead](https://cohost.org/virtueisdead)'s fault
---
+++ ppp.cpp
@@ -0,0 +1,10 @@
+#include "../common/pp_internal.h"
+
+#include <version.h>
+#include <plugin.h>
+#include <util.h>
+#include <debug.h>
+
+#define PLUGIN_ID		"garlic-pesterchum_pidgin_plugin"
+#define PLUGIN_AUTHOR	"garlicOS® <sisdfk@gmail.com>"
+#define PREFS_PREFIX	"/plugins/garlic/" PLUGIN_ID
