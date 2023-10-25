3aad5c9269e2207262c4f6841dc3ebe5a18a5100
Nate <14845347+garlic-os@users.noreply.github.com>
2023-10-24 22:52:40 -0300

    Refactor
    - include libpurple things from the submodule and not globally
    - Set proper GLIB_VERSION flag (I didn't know what I was doing b4)
    - Refactor a lot of variables
    - Remove prefs_info? not sure if i actually need it for what im doing but the code still doesnt compile so i cant know for sure
    - Add key names to struct initializations so you can actually tell what all these random values are
    - Comment out unused variables

--- libpesterchum.c
+++ libpesterchum.c
@@ -1,20 +1,39 @@
 #define PURPLE_PLUGINS
 
-#define GLIB_VERSION_MIN_REQUIRED (GLIB_VERSION_4_0)
+#define GLIB_VERSION_MIN_REQUIRED (GLIB_VERSION_2_76)
 #include <glib.h>
 
 #ifdef _WIN32
-	#include <win32dep.h>
+	#include "./libpurple-mini/win32/win32dep.h"
 #endif
 
-#include <version.h>
-#include <plugin.h>
-#include <util.h>
-#include <debug.h>
+#include "./libpurple-mini/version.h"
+#include "./libpurple-mini/plugin.h"
+#include "./libpurple-mini/util.h"
+#include "./libpurple-mini/internal.h"
+#include "./libpurple-mini/debug.h"
 
-#define PLUGIN_ID      "garlic-pesterchum_pidgin_plugin"
-#define PLUGIN_AUTHOR  "garlicOS® <sisdfk@gmail.com>"
-#define PREFS_PREFIX   "/plugins/garlic/" PLUGIN_ID
+
+#define P_NAME     "Purple Pesterchum"
+#define P_SUMMARY  "Pesterchum plugin for libpurple/Pidgin"
+#define P_DESC     "Renders Pesterchum effects in Pidgin."
+#define P_ID       "garlic-pesterchum_pidgin_plugin"
+#define P_AUTHOR   "garlicOS® <sisdfk@gmail.com>"
+#define P_VERSION  "0.0.1"
+#define P_WEBSITE  "https://github.com/garlic_os/purple-pesterchum"
+
+// TO DO: delete
+#define PREFS_I_RED    0
+#define PREFS_I_GREEN  0
+#define PREFS_I_BLUE   0
+#define PREFS_T_RED    0
+#define PREFS_T_GREEN  0
+#define PREFS_T_BLUE   0
+
+
+inline static guint8 round_gfloat_to_guint8(gfloat f) {
+	return ((guchar)(f + 0.5f));
+}
 
 
 static void colorize_message(char **message) {
@@ -61,11 +80,10 @@ static void colorize_message(char **message) {
 	                       round_gfloat_to_guint8(grad[2]));
 	g_string_append_unichar(new_msg, ch);
 
-	/* Format each character one by one:
-	*    (if it is printable and not a space -- this excludes LTR, control, and
-	*     format characters) AND
-	*    (if it is not the same color as the last character)
-	*/
+	// Format each character one by one:
+	//   (if it is printable and not a space -- this excludes LTR, control, and
+	//    format characters) AND
+	//   (if it is not the same color as the last character)
 	for ( ; c && *c; c = g_utf8_next_char(c)) {
 		gunichar ch = g_utf8_get_char(c);
 
@@ -100,66 +118,66 @@ static void colorize_message(char **message) {
 }
 
 
-static gboolean plugin_load(PurplePlugin *plugin) {
+static gboolean plugin_load(/* PurplePlugin *plugin */) {
 	return TRUE;
 }
 
 
-static gboolean plugin_unload(PurplePlugin *plugin) {
+static gboolean plugin_unload(/* PurplePlugin *plugin */) {
 	return TRUE;
 }
 
 
-static PurplePluginUiInfo prefs_info = {
-	init_pref_frame,
-	0,
-	NULL,
-	NULL,
-	NULL,
-	NULL,
-	NULL
-};
+// static PurplePluginUiInfo prefs_info = {
+// 	NULL,
+// 	0,
+// 	NULL,
+// 	NULL,
+// 	NULL,
+// 	NULL,
+// 	NULL
+// };
 
 static PurplePluginInfo info = {
-	PURPLE_PLUGIN_MAGIC,
-	PURPLE_MAJOR_VERSION,
-	PURPLE_MINOR_VERSION,
-	PURPLE_PLUGIN_STANDARD,
-	NULL,
-	0,
-	NULL,
-	PURPLE_PRIORITY_DEFAULT,
-	PLUGIN_ID,
-	NULL,
-	PP_VERSION,
-	NULL,
-	NULL,
-	PLUGIN_AUTHOR,
-	PP_WEBSITE,
-	plugin_load,
-	plugin_unload,
-	NULL,
-	NULL,
-	NULL,
-	&prefs_info,
-	NULL,
-	NULL,
-	NULL,
-	NULL,
-	NULL
+	.magic             = PURPLE_P_MAGIC,
+	.major_version     = PURPLE_MAJOR_VERSION,
+	.minor_version     = PURPLE_MINOR_VERSION,
+	.type              = PURPLE_P_STANDARD,
+	.ui_requirement    = NULL,
+	.flags             = 0,
+	.dependencies      = NULL,
+	.priority          = PURPLE_PRIORITY_DEFAULT,
+	.id                = P_ID,
+	.name              = NULL,
+	.version           = P_VERSION,
+	.summary           = NULL,
+	.description       = NULL,
+	.author            = P_AUTHOR,
+	.homepage          = P_WEBSITE,
+	.load              = plugin_load,
+	.unload            = plugin_unload,
+	.destroy           = NULL,
+	.ui_info           = NULL,
+	.extra_info        = NULL,
+	.prefs_info        = NULL, // &prefs_info,
+	.actions           = NULL,
+	._purple_reserved1 = NULL,
+	._purple_reserved2 = NULL,
+	._purple_reserved3 = NULL,
+	._purple_reserved4 = NULL
 };
 
 
 // Initialize default preferences
-static void init_plugin(PurplePlugin *plugin) {
+static void init_plugin(/* PurplePlugin *plugin */) {
 	#ifdef ENABLE_NLS
 		bindtextdomain(GETTEXT_PACKAGE, PP_LOCALEDIR);
 		bind_textdomain_codeset(GETTEXT_PACKAGE, "UTF-8");
 	#endif
-	info.name = _("PPP");
-	info.summary = _("Pesterchum Pidgin Plugin");
-	info.description = _("Renders Pesterchum effects in Pidgin.");
+	info.name = (char *) _(P_NAME);
+	info.summary = (char *) _(P_SUMMARY);
+	info.description = (char *) _(P_DESCRIPTION);
 }
 
 
-PURPLE_INIT_PLUGIN(ppp, init_plugin, info)
+PURPLE_INIT_PLUGIN(purple_pesterchum, init_plugin, info)
