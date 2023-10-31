e5fe814cc927bf5bb653d6fd3a14e0c793b5affb
Nate <14845347+garlic-os@users.noreply.github.com>
2023-10-31 18:52:26 -0300

    Refactor, add tests
    Finally I have achieved the test file layout that I wanted: just one function per test and the input and expected output right next to each other. No ugly garbage all around it

--- test/test.cpp
+++ test/test.cpp
@@ -2,21 +2,63 @@
 #include "../src/pesterchum.h"
 #include "./catch_amalgamated.hpp"
 
-using S = std::string;
-#define C(cstr) (const_cast<char*>(cstr))
 
-
-TEST_CASE("Color tags are converted", "[color]") {
-	char *message;
-	message = C("<c=#ff00ff>hi</c>");
+void do_test(std::string input, std::string expected_output) {
+	char *message = static_cast<char*>(malloc(input.size() + 1));
+	strcpy(message, input.c_str());
 	colorize_message(&message);
-	CHECK(S("<FONT COLOR=\"#ff00ff\">hi</FONT>") == message);
+	CHECK(expected_output == message);
+	free(message);
+}
 
-	message = C("guh<c=#ff00ff>hi</c>");
-	colorize_message(&message);
-	CHECK(S("guh<FONT COLOR=\"#ff00ff\">hi</FONT>") == message);
 
-	message = C("<c=#ff00ff>computer</c>");
-	colorize_message(&message);
-	CHECK(S("<FONT COLOR=\"#ff00ff\">computer</FONT>") == message);
+TEST_CASE("Color tags", "[color]") {
+	SECTION("Basic / hex code") {
+		do_test(
+			"<c=#ff00ff>hi</c>",
+			"<FONT COLOR=\"#ff00ff\">hi</FONT>"
+		);
+	}
+
+	SECTION("Longer text content") {
+		do_test(
+			"<c=#ff00ff>computer</c>",
+			"<FONT COLOR=\"#ff00ff\">computer</FONT"
+		);
+	}
+
+	SECTION("Text before tag") {
+		do_test(
+			"guh<c=#ff00ff>hi</c>",
+			"guh<FONT COLOR=\"#ff00ff\">hi</FONT>"
+		);
+	}
+
+	SECTION("Text after tag") {
+		do_test(
+			"<c=#ff00ff>hi</c>guh",
+			"<FONT COLOR=\"#ff00ff\">hi</FONT>guh"
+		);
+	}
+
+	SECTION("Multiple tags") {
+		do_test(
+			"<c=#ff00ff>hi</c><c=#00ff00>bye</c>",
+			"<FONT COLOR=\"#ff00ff\">hi</FONT><FONT COLOR=\"#00ff00\">bye</FONT>"
+		);
+	}
+
+	SECTION("RGB") {
+		do_test(
+			"<c=0,255,0>hi</c>",
+			"<FONT COLOR=\"rgb(0,255,0)\">hi</FONT>"
+		);
+	}
+
+	SECTION("Color name") {
+		do_test(
+			"<c=red>hi</c>",
+			"<FONT COLOR=\"red\">hi</FONT>"
+		);
+	}
 };
